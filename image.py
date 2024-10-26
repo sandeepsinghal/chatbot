cabda221473392cc2d306344784e3435c3747f82
Sandeep Singhal <sandeepsinghal@gmail.com>
2024-10-26 17:40:31 -0500

    migrating to the latest version using openapi migrate

--- image.py
+++ image.py
@@ -1,11 +1,11 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI()
 
 # Generate an image
-response = openai.Image.create(
-    prompt="A futuristic cityscape at sunset",
-    n=1,
-    size="1024x1024"
-)
+response = client.images.generate(prompt="A futuristic cityscape at sunset",
+                                  n=1,
+                                  size="1024x1024")
 # Get the URL of the generated image
-image_url = response['data'][0]['url']
+image_url = response.data[0].url
 print("Generated Image URL:", image_url)
